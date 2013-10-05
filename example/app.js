733dbabfa6772cc26a31101f0d1f550a4c8d2aa3
Ryutaro Miyashita <ryugoo@gmail.com>
2013-10-05 22:52:38 +0900

    bug fix

--- example/app.js
+++ example/app.js
@@ -18,31 +18,18 @@
     });
     win.add(start_detection);
 
-    var label1, label2, label3, label4, label5, label6, label7, label8;
-    label1 = Ti.UI.createLabel({
+    var label1, label2, label3, label4, label5, label6, label7, label8, opts;
+    opts = {
         top: 5
-    });
-    label2 = Ti.UI.createLabel({
-        top: 5
-    });
-    label3 = Ti.UI.createLabel({
-        top: 5
-    });
-    label4 = Ti.UI.createLabel({
-        top: 5
-    });
-    label5 = Ti.UI.createLabel({
-        top: 5
-    });
-    label6 = Ti.UI.createLabel({
-        top: 5
-    });
-    label7 = Ti.UI.createLabel({
-        top: 5
-    });
-    label8 = Ti.UI.createLabel({
-        top: 5
-    });
+    };
+    label1 = Ti.UI.createLabel(opts);
+    label2 = Ti.UI.createLabel(opts);
+    label3 = Ti.UI.createLabel(opts);
+    label4 = Ti.UI.createLabel(opts);
+    label5 = Ti.UI.createLabel(opts);
+    label6 = Ti.UI.createLabel(opts);
+    label7 = Ti.UI.createLabel(opts);
+    label8 = Ti.UI.createLabel(opts);
     win.add(label1);
     win.add(label2);
     win.add(label3);
@@ -76,9 +63,11 @@
         }
     };
 
+    var serialized = '{"glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}';
+
     var LOOP_LIMIT = 1000;
     function detection() {
-        var i, str, start, end;
+        var i, str, obj, start, end;
 
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
@@ -89,14 +78,20 @@
             text: 'Titanium stringify ' + (end - start) + ' [ms]'
         });
 
+        console.debug('===== Titanium stringify result =====');
+        console.debug(str);
+
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
-            JSON.parse(str);
+            obj = JSON.parse(serialized);
         }
         end = Date.now();
         label2.applyProperties({
             text: 'Titanium parse ' + (end - start) + ' [ms]'
         });
+
+        console.debug('===== Titanium parse result =====');
+        console.debug(obj);
 
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
@@ -107,14 +102,20 @@
             text: 'NSJSONSerialization stringify ' + (end - start) + ' [ms]'
         });
 
+        console.debug('===== NSJSONSerialization stringify result =====');
+        console.debug(str);
+
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
-            AltJSON.parse(str);
+            obj = AltJSON.parse(serialized);
         }
         end = Date.now();
         label4.applyProperties({
             text: 'NSJSONSerialization parse ' + (end - start) + ' [ms]'
         });
+
+        console.debug('===== NSJSONSerialization parse result =====');
+        console.debug(obj);
 
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
@@ -125,14 +126,20 @@
             text: 'JSONKit stringify ' + (end - start) + ' [ms]'
         });
 
+        console.debug('===== JSONKit stringify result =====');
+        console.debug(str);
+
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
-            AltJSON.parse2(str);
+            obj = AltJSON.parse2(serialized);
         }
         end = Date.now();
         label6.applyProperties({
             text: 'JSONKit parse ' + (end - start) + ' [ms]'
         });
+
+        console.debug('===== JSONKit parse result =====');
+        console.debug(obj);
 
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
@@ -143,14 +150,20 @@
             text: 'SBJson stringify ' + (end - start) + ' [ms]'
         });
 
+        console.debug('===== SBJson stringify result =====');
+        console.debug(str);
+
         start = Date.now();
         for (i = 0; i < LOOP_LIMIT; i++) {
-            AltJSON.parse3(str);
+            obj = AltJSON.parse3(serialized);
         }
         end = Date.now();
         label8.applyProperties({
             text: 'SBJson parse ' + (end - start) + ' [ms]'
         });
+
+        console.debug('===== SBJson parse result =====');
+        console.debug(obj);
     }
 
     win.addEventListener('open', detection);
